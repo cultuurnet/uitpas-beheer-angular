8307925261eea555151da7d21c955a2ecbca7c1f
Bert Ramakers <bertramakers@me.com>
2015-05-26 15:53:45 +0200

    Yeoman scaffolding.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uitpasbeheerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uitpasbeheerApp
+ */
+angular.module('uitpasbeheerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
