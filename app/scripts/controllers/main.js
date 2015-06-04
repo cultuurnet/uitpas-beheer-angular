9286aa67aa9f95c4ef6ffbde9393fcea33a35a5b
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-04 15:44:24 +0200

    UBR-55 re-added main.js

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uitpasbeheerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uitpasbeheerApp
+ */
+angular.module('uitpasbeheerApp')
+  .controller('MainCtrl', function () {
+
+  });
