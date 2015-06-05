bc7e58b9ae7a590161decd341c96e8ed756de869
Bert Ramakers <bertramakers@me.com>
2015-06-05 13:01:24 +0200

    UBR-45: loading view and app-loading directive.

---
+++ app/scripts/directives/apploading.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name uitpasbeheerApp.directive:appLoading
+ * @description
+ * # appLoading
+ */
+angular
+  .module('uitpasbeheerApp')
+  .directive('appLoading', function ($rootScope) {
+    return {
+      templateUrl: 'views/loading.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        scope.loading = true;
+
+        $rootScope.$watch('appReady', function() {
+          scope.loading = !$rootScope.appReady;
+        });
+      }
+    };
+  });
