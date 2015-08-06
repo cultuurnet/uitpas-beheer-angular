144381a33ac2adc11997b7d6d3d2d1f83a0c2c77
Sander Vleugels <sandervleugels@gmail.com>
2015-08-06 16:21:56 +0200

    UBR-71: stricter inszNumber validation

--- app/scripts/controllers/passholderEdit.js
+++ app/scripts/controllers/passholderEdit.js
@@ -49,24 +49,4 @@ function PassholderEditController (passholder, identification, $modalInstance, p
   controller.cancelModal = function() {
     $modalInstance.dismiss();
   };
-
-  controller.validateInszNumber = (function() {
-    // examples: 930518-223-61, 93051822361, 930518 223 61, 930518.223.61
-    var regex = /([\d]{6})[-.\s]?([\d]{3})[-.\s]?([\d]{2}$)/;
-
-    return {
-      test: function(value) {
-        var inszIsValid = false;
-        var regexResult = regex.exec(value);
-        if (regexResult) {
-          var rest = (regexResult[1] + regexResult[2]) % 97;
-          inszIsValid = (String(97 - rest) === regexResult[3]);
-        }
-        else {
-          inszIsValid = false;
-        }
-        return inszIsValid;
-      }
-    };
-  })();
 }
