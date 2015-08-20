b73e983ded0c55bd22f3e83db0c8d4038265cab8
Sander Vleugels <sandervleugels@gmail.com>
2015-08-20 15:00:52 +0200

    UBR-75: extra point bounce directive test

--- test/spec/directives/passholder-points.directive.js
+++ test/spec/directives/passholder-points.directive.js
@@ -33,6 +33,39 @@ describe('Directive: udbPassholderPoints', function () {
     expect($animate.removeClass.calls.count()).toEqual(2);
   }));
 
+  it('should pulse when the passholder saves points', inject(function ($rootScope, $compile) {
+    var deferredAnimation = $q.defer();
+    var animationPromise = deferredAnimation.promise;
+
+    element = $compile('<span ubr-passholder-points></span>')(scope);
+    scope.$digest();
+
+    spyOn($animate, 'removeClass').and.callThrough();
+    spyOn($animate, 'addClass').and.returnValue(animationPromise);
+
+    $rootScope.$emit('activityCheckedIn', {
+      'id': 'e71f3381-21aa-4f73-a860-17cf3e31f013',
+      'title': 'Altijd open',
+      'description': '',
+      'when': '',
+      'points': 182,
+      'checkinConstraint': {
+        'allowed': true,
+        'startDate': '2015-09-01T00:00:00+00:00',
+        'endDate': '2015-09-01T23:59:59+00:00',
+        'reason': ''
+      }
+    });
+    scope.$digest();
+
+    expect($animate.removeClass).toHaveBeenCalledWith(jasmine.any(Object), 'animated pulse');
+    expect($animate.addClass).toHaveBeenCalledWith(jasmine.any(Object), 'animated pulse');
+
+    deferredAnimation.resolve();
+    scope.$digest();
+    expect($animate.removeClass.calls.count()).toEqual(2);
+  }));
+
   it('should not pulse when a free advantage is exchanged', inject(function ($rootScope, $compile) {
     var deferredAnimation = $q.defer();
     var animationPromise = deferredAnimation.promise;
