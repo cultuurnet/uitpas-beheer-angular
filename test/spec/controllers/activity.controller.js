dc2ff133be3aee94b1d380eff66925ca11e5a0cc
Sander Vleugels <sandervleugels@gmail.com>
2015-08-20 15:37:18 +0200

    UBR-75: extra activity controller test

--- test/spec/controllers/activity.controller.js
+++ test/spec/controllers/activity.controller.js
@@ -45,30 +45,33 @@ describe('Controller: ActivityController', function () {
       activityService: activityService,
       DateRange: DateRange
     });
-
-    deferredActivities.resolve(pagedActivities);
-    $scope.$digest();
   }));
 
   it('should fetch an initial list of activities for the active passholder', function () {
+    deferredActivities.resolve(pagedActivities);
+    $scope.$digest();
+
     expect(activityService.search).toHaveBeenCalled();
     expect(activityController.activitiesLoading).toEqual(0);
   });
 
   it('should reset the active page when the query or date range search parameter changes', function () {
-    // reset when the query changes
+    deferredActivities.resolve(pagedActivities);
+    $scope.$digest();
+
+    // Reset when the query changes.
     activityController.page = 3;
     activityController.query = 'some new query';
     activityController.searchParametersChanged();
     expect(activityController.page).toEqual(1);
 
-    // reset when date range changes
+    // Reset when date range changes.
     activityController.page = 4;
     activityController.dateRange = DateRange.PAST;
     activityController.searchParametersChanged();
     expect(activityController.page).toEqual(1);
 
-    // do not reset when none of the above change
+    // Do not reset when none of the above change.
     activityController.page = 2;
     activityController.searchParametersChanged();
     expect(activityController.page).toEqual(2);
@@ -90,6 +93,9 @@ describe('Controller: ActivityController', function () {
   });
 
   it('should enter a loading state while retrieving search results', function (){
+    deferredActivities.resolve(pagedActivities);
+    $scope.$digest();
+
     activityController.search();
     expect(activityController.activitiesLoading).toEqual(1);
 
@@ -100,7 +106,7 @@ describe('Controller: ActivityController', function () {
 
   it('should exit the loading state when search results fail to load', function (){
     activityController.search();
-    expect(activityController.activitiesLoading).toEqual(1);
+    expect(activityController.activitiesLoading).toEqual(2);
 
     deferredActivities.reject();
     $scope.$digest();
@@ -108,6 +114,9 @@ describe('Controller: ActivityController', function () {
   });
 
   it('should check in a passholder to an activity', function () {
+    deferredActivities.resolve(pagedActivities);
+    $scope.$digest();
+
     var deferredCheckin = $q.defer();
 
     var activity = {
@@ -139,6 +148,9 @@ describe('Controller: ActivityController', function () {
   });
 
   it('should set error values when checking in a passholder to an activity fails', function () {
+    deferredActivities.resolve(pagedActivities);
+    $scope.$digest();
+
     var deferredCheckin = $q.defer();
 
     var activity = {
