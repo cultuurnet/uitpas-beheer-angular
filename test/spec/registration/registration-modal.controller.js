af8525c056b1cf89bf22310bdd1608a40c1625f2
Sander Vleugels <sandervleugels@gmail.com>
2015-09-01 15:54:42 +0200

    UBR-139: add test for the registration modal

---
+++ test/spec/registration/registration-modal.controller.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('Controller: PassholderRegisterController', function () {
+
+  beforeEach(module('ubr.registration'));
+
+  var controller, Pass, unregisteredPass, $state, Passholder, passholderService, $scope, $controller, modalInstance, counterService;
+
+  var unregisteredPassData = {
+    'uitPas': {
+      number: '0930000422202',
+      kansenStatuut: false,
+      status: 'ACTIVE',
+      type: 'CARD'
+    }
+  };
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($injector, $rootScope) {
+    modalInstance = {
+      close: jasmine.createSpy('modalInstance.close'),
+      dismiss: jasmine.createSpy('modalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('modalInstance.result.then')
+      }
+    };
+
+    $controller = $injector.get('$controller');
+    Pass = $injector.get('Pass');
+    unregisteredPass = new Pass(unregisteredPassData);
+    $scope = $rootScope;
+
+    $state = $injector.get('$state');
+    Passholder = $injector.get('Passholder');
+    passholderService = $injector.get('passholderService');
+    counterService = $injector.get('counterService');
+
+    controller = $controller('RegistrationModalController', {
+      pass: unregisteredPass,
+      $state: $state,
+      Passholder: Passholder,
+      passholderService: passholderService,
+      $modalInstance: modalInstance,
+      counterService: counterService
+    });
+  }));
+
+  it('should have a pass object', function () {
+    expect(controller.pass).toEqual(unregisteredPass);
+    expect(controller.formSubmitBusy).toBeFalsy();
+    expect(controller.price).toEqual(0);
+  });
+});
