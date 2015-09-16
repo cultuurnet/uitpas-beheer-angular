d077b344d3bc38aa6065b837fd67bdb21c058b57
Bram Cordie <bramcordie@gmail.com>
2015-09-16 15:41:10 +0200

    UBR-179: Use patch to update passholders

--- test/spec/services/passholder.js
+++ test/spec/services/passholder.js
@@ -190,7 +190,7 @@ describe('Service: passholderService', function () {
     var expectedPassholderData = passholderPostData.serialize();
 
     $httpBackend
-      .expectPOST(apiUrl + 'passholders/' + uitpasNumber, expectedPassholderData)
+      .expectPATCH(apiUrl + 'passholders/' + uitpasNumber, expectedPassholderData)
       .respond(200, JSON.stringify(expectedPassholderData));
 
     $httpBackend
@@ -230,7 +230,7 @@ describe('Service: passholderService', function () {
     };
 
     $httpBackend
-      .expectPOST(apiUrl + 'passholders/' + uitpasNumber, passholderPostData.serialize())
+      .expectPATCH(apiUrl + 'passholders/' + uitpasNumber, passholderPostData.serialize())
       .respond(403, JSON.stringify(expectedAPIError));
 
     var assertRejectedWithError = function (response) {
@@ -262,7 +262,7 @@ describe('Service: passholderService', function () {
     };
 
     $httpBackend
-      .expectPOST(apiUrl + 'passholders/' + uitpasNumber, passholderPostData.serialize())
+      .expectPATCH(apiUrl + 'passholders/' + uitpasNumber, passholderPostData.serialize())
       .respond(403, JSON.stringify(expectedAPIError));
 
     var assertRejectedWithError = function (response) {
