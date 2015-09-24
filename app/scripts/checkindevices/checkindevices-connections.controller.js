4eebebbfa2b8d8093ed804f3915f8ad410b220cc
Kristof Coomans <kristof@2dotstwice.be>
2015-09-24 11:20:24 +0200

    UBR-249: Add basic setup of states, controllers and views (dummy HTML copied from prototype)

---
+++ app/scripts/checkindevices/checkindevices-connections.controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ubr.group.controller:GroupActivityController
+ * @description
+ * # GroupActivityController
+ * Controller of the ubr group module.
+ */
+angular
+  .module('ubr.checkindevices')
+  .controller('CheckInDevicesConnectionsController', CheckInDevicesConnectionsController);
+
+/* @ngInject */
+function CheckInDevicesConnectionsController () {
+  /*jshint validthis: true */
+  var controller = this;
+}
---
+++ app/scripts/checkindevices/ubr.checkindevices.module.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ubr.checkindevices
+ * @description
+ * # ubr.checkindevices
+ *
+ * checkindevices module UiTPAS Beheer
+ */
+angular
+  .module('ubr.checkindevices', [
+    'ui.router',
+    'uitpasbeheerApp',
+    'truncate'
+  ])
+  /* @ngInject */
+  .config(function ($stateProvider) {
+
+    $stateProvider.state(
+      'counter.checkindevices',
+      {
+        url: '/checkindevices',
+        requiresCounter: true,
+        views: {
+          content: {
+            templateUrl: 'views/checkindevices/content-checkindevices.html',
+            controller: 'CheckInDevicesConnectionsController',
+            controllerAs: 'cidc'
+          },
+          sidebar: {
+            templateUrl: 'views/checkindevices/sidebar-checkindevices.html',
+            controller: 'CheckInDevicesConnectionsController',
+            controllerAs: 'cidc'
+          },
+          header: {
+            templateUrl: 'views/header.html'
+          }
+        }
+      }
+    );
+  });
