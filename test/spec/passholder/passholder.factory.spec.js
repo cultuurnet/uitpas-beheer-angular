8b88653f12d4ec784269ac1451902ee85ec0d1a2
Sander Vleugels <sandervleugels@gmail.com>
2015-10-27 11:38:47 +0100

    UBR-305: add tests for the Passholder factory.

--- test/spec/passholder/passholder.factory.spec.js
+++ test/spec/passholder/passholder.factory.spec.js
@@ -7,7 +7,8 @@ describe('Factory: Passholder', function () {
   var jsonPassholder = {
     'name': {
       'first': 'Victor',
-      'last': 'D\'Hooghe'
+      'last': 'DHooghe',
+      'middle': 'Vikky'
     },
     'address': {
       'street': 'Baanweg 60',
@@ -58,7 +59,8 @@ describe('Factory: Passholder', function () {
     var expectedPassholder = {
       name: {
         first: 'Victor',
-        last: 'D\'Hooghe'
+        last: 'DHooghe',
+        middle: 'Vikky'
       },
       address: {
         street: 'Baanweg 60',
@@ -99,7 +101,6 @@ describe('Factory: Passholder', function () {
     var passholder = new Passholder(jsonPassholder);
 
     expect(passholder).toEqual(expectedPassholder);
-
   });
 
   it('should overwrite existing contact info with defaults when parsing data without a contact property', function() {
@@ -143,5 +144,134 @@ describe('Factory: Passholder', function () {
 
     passholder.parseJson(updatedPassholderData);
     expect(passholder.kansenStatuten).toEqual(expectedKansenstatuten);
+  });
+
+  it('should correctly parse a passholder with uitPassen', function () {
+    var jsonPassholder = getJsonPassholder();
+    jsonPassholder.uitpassen = [
+      {
+        'number': '0930000422202',
+        'kansenStatuut': false,
+        'status': 'ACTIVE',
+        'type': 'CARD',
+        'cardSystem': {
+          name: 'UiTPAS Regio Aalst',
+          id: '1'
+        }
+      }
+    ];
+    jsonPassholder.inszNumber = 'insz';
+
+    var expectedPassholder = {
+      name: {
+        first: 'Victor',
+        last: 'DHooghe',
+        middle: 'Vikky'
+      },
+      address: {
+        street: 'Baanweg 60',
+        postalCode: '9308',
+        city: 'Aalst'
+      },
+      birth: {
+        date: day('2007-11-15', 'YYYY-MM-DD').toDate(),
+        place: 'Aalst'
+      },
+      gender: 'MALE',
+      nationality: 'belg',
+      privacy: {
+        email: false,
+        sms: false
+      },
+      contact: {
+        email: 'email@email.com',
+        telephoneNumber: '',
+        mobileNumber: ''
+      },
+      kansenStatuten: [{
+        status: 'ACTIVE',
+        endDate: day('2015-12-06', 'YYYY-MM-DD').toDate(),
+        cardSystem: {
+          name: 'UiTPAS Regio Aalst',
+          id: '1'
+        }
+      }],
+      points: 309,
+      picture: 'data:image/jpeg;base64, ' + 'picture-in-base64-format',
+      inszNumber: 'insz',
+      remarks: 'remarks',
+      uid: 'e1e2b335-e756-4e72-bb0f-3d163a583b35',
+      uitPassen: [
+        {
+          'number': '0930000422202',
+          'kansenStatuut': false,
+          'status': 'ACTIVE',
+          'type': 'CARD',
+          'cardSystem': {
+            name: 'UiTPAS Regio Aalst',
+            id: '1'
+          }
+        }
+      ]
+    };
+
+    var passholder = new Passholder(jsonPassholder);
+
+    expect(passholder).toEqual(expectedPassholder);
+  });
+
+  it('has a helper function to return the full name', function () {
+    var expectedFullName = 'Victor Vikky DHooghe';
+    var passholder = new Passholder(getJsonPassholder());
+
+    expect(passholder.getFullName()).toEqual(expectedFullName);
+  });
+
+  it('has a helper function to serialize the passholder', function () {
+    var expectedSerializedData = {
+      name: {
+        first: 'Victor',
+        last: 'DHooghe',
+        middle: 'Vikky'
+      },
+      address: {
+        street: 'Baanweg 60',
+        postalCode: '9308',
+        city: 'Aalst'
+      },
+      birth: {
+        date: '2007-11-15',
+        place: 'Aalst'
+      },
+      inszNumber: '',
+      picture: 'data:image/jpeg;base64, ' + 'picture-in-base64-format',
+      gender: 'MALE',
+      nationality: 'belg',
+      privacy: {
+        email: false,
+        sms: false
+      },
+      contact: {
+        email: 'email@email.com',
+        telephoneNumber: '',
+        mobileNumber: ''
+      },
+      points: 309,
+      kansenStatuten: [
+        {
+          status: 'ACTIVE',
+          endDate: '2015-12-06',
+          cardSystem: {
+            id: '1',
+            name: 'UiTPAS Regio Aalst'
+          }
+        }
+      ],
+      remarks: 'remarks',
+      uid: 'e1e2b335-e756-4e72-bb0f-3d163a583b35'
+    };
+    var passholder = new Passholder(getJsonPassholder());
+
+    expect(passholder.serialize()).toEqual(expectedSerializedData);
   });
 });
