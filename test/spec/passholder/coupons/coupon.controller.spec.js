124d2be9f191c019f907bc8ce8b52f11234b9e99
Bram Cordie <bramcordie@gmail.com>
2015-11-05 13:47:01 +0100

    UBR-348: Fix coupon controller test

--- test/spec/passholder/coupons/coupon.controller.spec.js
+++ test/spec/passholder/coupons/coupon.controller.spec.js
@@ -1,31 +1,31 @@
 'use strict';
 
 describe('Controller: CouponDetailController', function () {
+  var couponDetailController, coupon, $controller, $modalInstance, $scope;
+
   // load the controller's module
   beforeEach(module('ubr.passholder'));
   beforeEach(module('uitpasbeheerApp'));
 
-  beforeEach(spyCoupon());
-
-  var couponDetailController, coupon, $controller, $modalInstance;
-
-  function spyCoupon() {
+  beforeEach(inject(function (_$controller_, $rootScope) {
+    $controller = _$controller_;
     $modalInstance = jasmine.createSpyObj('$modalInstance', ['dismiss']);
-  }
-
-  couponDetailController = getController();
-  coupon = {
-    id: '0',
+    $scope = $rootScope.$new();
+    couponDetailController = getController();
+    coupon = {
+      id: '0',
       name: 'Cultuurbon',
       description: 'Dit aanbod is geldig voor elke pashouder met een Paspartoe aan reductieprijs.',
       date: '2015-12-26',
       remainingTotal: 4
-  };
+    };
+  }));
 
   function getController() {
     return $controller('CouponDetailController', {
       coupon: coupon,
-      $modalInstance: $modalInstance
+      $modalInstance: $modalInstance,
+      $scope: $scope
     });
   }
 
