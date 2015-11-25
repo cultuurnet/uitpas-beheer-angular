e0ebe79872b4b03ed29a96e1fc4cfe499118b662
Sander Vleugels <sandervleugels@gmail.com>
2015-11-25 16:40:29 +0100

    UBR-360: change " to '

--- test/spec/advantage/advantage-detail.controller.spec.js
+++ test/spec/advantage/advantage-detail.controller.spec.js
@@ -10,44 +10,44 @@ describe('Controller: PassholderAdvantageDetailController', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $injector) {
     advantage = {
-      "id": "welcome--391",
-      "title": "Korting op koffie.",
-      "points": 50,
-      "exchangeable": true,
-      "description1": "description 1: Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec sed odio dui.",
-      "description2": "",
-      "validForCities": [],
-      "validForCounters": [
+      'id': 'welcome--391',
+      'title': 'Korting op koffie.',
+      'points': 50,
+      'exchangeable': true,
+      'description1': 'description 1: Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec sed odio dui.',
+      'description2': '',
+      'validForCities': [],
+      'validForCounters': [
         {
-          "id": "390",
-          "name": "CC De Werf",
-          "consumerKey": "31413BDF-DFC7-7A9F-10403618C2816E44",
-          "role": "admin",
-          "actorId": "31413BDF-DFC7-7A9F-10403618C2816E44",
-          "cardSystems": [
+          'id': '390',
+          'name': 'CC De Werf',
+          'consumerKey': '31413BDF-DFC7-7A9F-10403618C2816E44',
+          'role': 'admin',
+          'actorId': '31413BDF-DFC7-7A9F-10403618C2816E44',
+          'cardSystems': [
             {
-              "permissions": [
-                "registration"
+              'permissions': [
+                'registration'
               ],
-              "groups": [
-                "Can create passholders for other municipalities"
+              'groups': [
+                'Can create passholders for other municipalities'
               ],
-              "id": 1,
-              "name": "UiTPAS Regio Aalst",
-              "distributionKeys": [
-                "string"
+              'id': 1,
+              'name': 'UiTPAS Regio Aalst',
+              'distributionKeys': [
+                'string'
               ]
             }
           ],
-          "permissions": [
-            "registration"
+          'permissions': [
+            'registration'
           ],
-          "groups": [
-            "Can create passholders for other municipalities"
+          'groups': [
+            'Can create passholders for other municipalities'
           ]
         }
       ],
-      "endDate": "2016/09/10"
+      'endDate': '2016/09/10'
     };
 
     $uibModalInstance = {
