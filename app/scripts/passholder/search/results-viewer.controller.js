66bd8aff92e4588dbf40099662ea4dfcb784a02f
Sander Vleugels <sandervleugels@gmail.com>
2015-11-30 16:02:34 +0100

    UBR-410: add functionality to the result viewer controller

--- app/scripts/passholder/search/results-viewer.controller.js
+++ app/scripts/passholder/search/results-viewer.controller.js
@@ -12,18 +12,29 @@ angular
   .controller('ResultsViewerController', ResultsViewerController);
 
 /* @ngInject */
-function ResultsViewerController (advancedSearchService, $rootScope, $scope, $state, SearchParameters, UiTPASRouter) {
+function ResultsViewerController (advancedSearchService, $rootScope, $scope, $state, SearchParameters, UiTPASRouter, BulkSelection, PassholderSearchResults) {
+  function getSearchParametersFromState() {
+    var params = new SearchParameters();
+    params.fromParams($state.params);
+
+    return params;
+  }
+
   /*jshint validthis: true */
   var controller = this;
   controller.activePage = 1;
   controller.loading = true;
-
   /** @type {PassholderSearchResults} */
-  controller.results = null;
+  controller.results = new PassholderSearchResults();
+  controller.searchParameters = getSearchParametersFromState();
+  controller.bulk = {
+    action: 'export',
+    submitBusy: false,
+    selection: new BulkSelection(controller.results, controller.searchParameters)
+  };
 
   controller.hasDefaultParameters = function () {
-    var searchParameters = new SearchParameters();
-    searchParameters.fromParams($state.params);
+    var searchParameters = getSearchParametersFromState();
     return searchParameters.hasDefaultParameters();
   };
 
@@ -32,7 +43,7 @@ function ResultsViewerController (advancedSearchService, $rootScope, $scope, $st
   }
 
   controller.noSearchDone = function () {
-    return !controller.results && controller.hasDefaultParameters();
+    return (controller.results.totalItems === 0 && controller.hasDefaultParameters());
   };
 
   controller.isShowingResults = function () {
@@ -43,7 +54,7 @@ function ResultsViewerController (advancedSearchService, $rootScope, $scope, $st
       showResults = !results.hasUnknownNumbers() || results.hasConfirmedUnknownNumbers();
     }
 
-    if (!results && !controller.hasDefaultParameters()) {
+    if (results.totalItems === 0 && !controller.hasDefaultParameters()) {
       showResults = true;
     }
 
@@ -72,7 +83,40 @@ function ResultsViewerController (advancedSearchService, $rootScope, $scope, $st
   controller.updateResults = function (event, searchResults) {
     controller.results = searchResults;
     controller.updateActivePage(searchResults.page);
+    // Update the search results in the bulk selection object.
+    controller.bulk.selection.updateSearchResults(searchResults);
     controller.loading = false;
+  };
+
+  /**
+   * Watch the click action for the select all checkbox.
+   */
+  controller.bulkSelectAll = function () {
+    // Clear the selection if the checkbox is deselected.
+    if (controller.bulk.selection.selectAll === false) {
+      controller.bulk.selection.removeAllUitpasNumbers();
+    }
+  };
+
+  /**
+   * Change the selection of a given pass. Can be added or removed depending on the current selection.
+   *
+   * @param {Pass} pass
+   */
+  controller.togglePassBulkSelection = function (pass) {
+    // Remove the pass if all passes are selecter or if the pass is in the selection.
+    if (controller.bulk.selection.selectAll || controller.bulk.selection.numberInSelection(pass.number)) {
+      controller.bulk.selection.removeUitpasNumberFromSelection(pass.number);
+    }
+    else {
+      controller.bulk.selection.addUitpasNumberToSelection(pass.number);
+    }
+  };
+
+  controller.doBulkAction = function () {
+    console.log(controller.bulk.action);
+    console.log(controller.bulk.selection.toBulkSelection());
+    controller.bulk.submitBusy = true;
   };
 
   /**
@@ -94,6 +138,8 @@ function ResultsViewerController (advancedSearchService, $rootScope, $scope, $st
     controller.loading = true;
 
     lastSearchParameters = searchParameters;
+    // Update the search parameters in the bulk selection object.
+    controller.bulk.selection.updateSearchParameters(searchParameters);
   };
 
   var searchResultsListener = $rootScope.$on('passholdersFound', controller.updateResults);
