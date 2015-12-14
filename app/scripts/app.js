3965ee7124d28eac0d6f3690703e7066e58c8471
Sander Vleugels <sandervleugels@gmail.com>
2015-12-14 16:21:32 +0100

    UBR-416: remove code that breaks the registration modal flow

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -123,15 +123,7 @@ angular
     $locationProvider.html5Mode(true);
     $httpProvider.defaults.withCredentials = true;
   })
-  .run(function(nfcService, eIDService, $rootScope, $uibModalStack) {
-    $rootScope.$on('$locationChangeStart', function(event){
-      var top = $uibModalStack.getTop();
-      // if a modal window is still open close it
-      if (top) {
-        event.preventDefault();
-        $uibModalStack.dismiss(top.key);
-      }
-    });
+  .run(function(nfcService, eIDService) {
     nfcService.init();
     eIDService.init();
   })
