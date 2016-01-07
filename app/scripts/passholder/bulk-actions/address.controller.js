cbf73ec54bfda23486e53c92610bf20bf1650ea5
Stijn Swaanen <swaanenstijn@gmail.com>
2016-01-07 14:51:01 +0100

    UBR-327 make bulk update work.

--- app/scripts/passholder/bulk-actions/address.controller.js
+++ app/scripts/passholder/bulk-actions/address.controller.js
@@ -12,13 +12,55 @@ angular
   .controller('AddressBulkController', AddressBulkController);
 
 /* @ngInject */
-function AddressBulkController (bulkSelection, $uibModalInstance) {
+function AddressBulkController (bulkSelection, advancedSearchService, passholderService, $uibModalInstance) {
   var controller = this;
   controller.submitBusy = false;
+  controller.bulkSelection = bulkSelection;
+  var searchParameters = bulkSelection.searchParameters;
+  var totalItems = bulkSelection.searchResults.totalItems;
 
-  controller.submitForm = function() {
-    controller.submitBusy = true;
-    console.log(controller);
+  if (bulkSelection.selectAll) {
+    searchParameters.limit = totalItems;
+    findPassHoldersAgain(searchParameters);
+  }
+  else {
+    var passnumbers = bulkSelection.uitpasNumberSelection;
+  }
+
+
+  controller.showSearchResults = function (searchResults) {
+    controller.reloadedResults = searchResults;
+  };
+
+  function findPassHoldersAgain(searchParameters) {
+    advancedSearchService
+      .findPassholders(searchParameters)
+      .finally(controller.showSearchResults);
+  }
+
+  function updatePassHolder(bulkAddressForm, passnumber){
+    passholderService.findPassholder(passnumber).then(
+      function (passholder) {
+        passholder.address.city = bulkAddressForm.city.$viewValue;
+        passholder.address.postalCode = bulkAddressForm.zip.$viewValue;
+        passholder.address.street = bulkAddressForm.street.$viewValue;
+        passholderService.update(passholder, passnumber);
+      }
+    );
+  }
+
+  controller.submitForm = function(bulkAddressForm) {
+    if (!controller.submitBusy) {
+
+      /*var street =;
+      var zip =;
+      var city = ;*/
+
+      controller.submitBusy = true;
+      for (var i = 0, len = passnumbers.length; i < len; i++) {
+        updatePassHolder(bulkAddressForm, passnumbers[i]);
+      }
+    }
   };
 
   this.cancel = function () {
