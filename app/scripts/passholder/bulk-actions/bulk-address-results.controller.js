aa1864e492d92aa431a73cb422ff37096a9be6ca
Stijn Swaanen <swaanenstijn@gmail.com>
2016-01-13 15:27:18 +0100

    UBR-327 first try of catching the errors when bulk updating addresses.

--- app/scripts/passholder/bulk-actions/bulk-address-results.controller.js
+++ app/scripts/passholder/bulk-actions/bulk-address-results.controller.js
@@ -11,22 +11,51 @@ angular
   .module('ubr.passholder.bulkActions')
   .controller('AddressBulkResultsController', AddressBulkResultsController);
 
-function AddressBulkResultsController(passholders, bulkAddressForm, passholderService, $uibModalInstance) {
+function AddressBulkResultsController(passholders, bulkAddressForm, passholderService, $uibModalInstance, $rootScope) {
   var controller = this;
   controller.submitBusy = true;
   controller.passholders = passholders;
+  controller.asyncError = null;
 
   for (var i = 0, len = controller.passholders.length; i < len; i++) {
     controller.passholders[i].updated = false;
     updatePassHolder(controller.passholders[i]);
-    controller.passholders[i].updated = true;
+    //controller.passholders[i].updated = true;
   }
 
   function updatePassHolder(passholder){
     passholder.address.city = bulkAddressForm.city.$viewValue;
     passholder.address.postalCode = bulkAddressForm.zip.$viewValue;
     passholder.address.street = bulkAddressForm.street.$viewValue;
-    console.log(passholderService.update(passholder, passholder.passNumber));
+
+    var updateOk = function(updatedPassholder) {
+      updatedPassholder.updated = true;
+      //$uibModalInstance.close();
+    };
+
+    var updateFailed = function(errorResponse) {
+      var errorCode = errorResponse.apiError.code;
+      // clear any previous async errors
+      controller.asyncError = null;
+
+
+      if (errorCode === 'ACTION_NOT_ALLOWED') {
+        controller.asyncError = {
+          message: 'Actie niet toegestaan.',
+          type: 'danger'
+        };
+      }
+      
+      if (errorCode === 'PASSHOLDER_NOT_UPDATED_ON_SERVER') {
+        controller.asyncError = {
+          message: errorResponse.apiError.message,
+          type: 'danger'
+        };
+      }
+    };
+
+    passholderService.update(passholder, passholder.passNumber)
+      .then(updateOk, updateFailed);
   }
 
   this.cancel = function() {
