59c27ba88bc5603e9b9767e0f019907cfebe510d
Sander Vleugels <sandervleugels@gmail.com>
2016-01-25 14:05:11 +0100

    UBR-330: prepare for more testing of the card upgrade controller

--- test/spec/passholder/card-upgrade/card-upgrade-modal.controller.spec.js
+++ test/spec/passholder/card-upgrade/card-upgrade-modal.controller.spec.js
@@ -22,11 +22,100 @@ describe('Controller: Card Upgrade Modal', function () {
     });
   }));
 
+  var jsonUitpasBase = {
+    'number': '0930000422202',
+    'kansenStatuut': false,
+    'status': 'ACTIVE',
+    'type': 'CARD',
+    cardSystem: {
+      name: 'UiTPAS Regio Aalst',
+      id: '1'
+    }
+  };
+
+  var jsonPassholderBase = {
+    'name': {
+      'first': 'Victor',
+      'last': 'DHooghe'
+    },
+    'address': {
+      'street': 'Baanweg 60',
+      'postalCode': '9308',
+      'city': 'Aalst'
+    },
+    'birth': {
+      'date': '2007-11-15',
+      'place': 'Aalst'
+    },
+    'gender': 'MALE',
+    'nationality': 'belg',
+    'privacy': {
+      'email': false,
+      'sms': false
+    },
+    'contact': {
+      'email': 'email@email.com'
+    },
+    kansenStatuten: [{
+      status: 'ACTIVE',
+      endDate: '2015-12-06',
+      cardSystem: {
+        name: 'UiTPAS Regio Aalst',
+        id: '1'
+      }
+    }],
+    'points': 309,
+    'remarks': 'remarks',
+    'uid': 'e1e2b335-e756-4e72-bb0f-3d163a583b35',
+    'uitpassen': [
+      {
+        'number': '0930000422202',
+        'kansenStatuut': false,
+        'status': 'ACTIVE',
+        'type': 'CARD',
+        'cardSystem': {
+          name: 'UiTPAS Regio Aalst',
+          id: '1'
+        }
+      }
+    ]
+  };
+
+  function getJsonPassWithPassholder() {
+    return {
+      'uitPas': angular.copy(jsonUitpasBase),
+      'passHolder': angular.copy(jsonPassholderBase)
+    };
+  }
+
+  var jsonCounter = {
+    'id': '452',
+    'consumerKey': 'b95d1bcf-533d-45ac-afcd-e015cfe86c84',
+    'name': 'Vierdewereldgroep Mensen voor Mensen',
+    'role': 'admin',
+    'actorId': 'b95d1bcf-533d-45ac-afcd-e015cfe86c84',
+    'cardSystems': {
+      '1': {
+        'permissions': ['registratie', 'kansenstatuut toekennen'],
+        'groups': ['Geauthorizeerde registratie balies'],
+        'id': 1,
+        'name': 'UiTPAS Regio Aalst',
+        'distributionKeys': []
+      }
+    },
+    'permissions': ['registratie', 'kansenstatuut toekennen'],
+    'groups': ['Geauthorizeerde registratie balies']
+  };
+
+  function getJsonCounter(){
+    return angular.copy(jsonCounter);
+  }
+
   beforeEach(inject(function (_$controller_, $injector, _$rootScope_) {
     Pass = $injector.get('Pass');
-    pass = new Pass();
+    pass = new Pass(getJsonPassWithPassholder());
     Counter = $injector.get('Counter');
-    activeCounter = new Counter();
+    activeCounter = new Counter(getJsonCounter());
     cardSystem = {
       id: 1
     };
@@ -39,6 +128,13 @@ describe('Controller: Card Upgrade Modal', function () {
       stepNumber: 1
     };
     spyOn($state, 'go');
+    $uibModalInstance = {
+      close: jasmine.createSpy('modalInstance.close'),
+      dismiss: jasmine.createSpy('modalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('modalInstance.result.then')
+      }
+    };
 
     $controller = _$controller_;
     $rootScope = _$rootScope_;
@@ -64,6 +160,51 @@ describe('Controller: Card Upgrade Modal', function () {
   }
 
   it('can determine which modal flow to used based on card system', function () {
+    expect(controller.modalFlow).toBe('AUTHORIZED_COUNTER');
+
+    controller.showModalFlowForCardSystem(100);
+    expect(controller.modalFlow).toBe('UNAUTHORIZED_COUNTER');
+    expect(controller.upgradeData.withNewCard).toBe('NEW_CARD');
+  });
+
+  it('can dismiss the modal', function () {
+    controller.close();
+    expect($uibModalInstance.dismiss).toHaveBeenCalled();
+  });
+
+  it('can submit the kansenstatuut form', function () {
+
+  });
+
+  it('should not submit the kansenstatuut form when there are errors', function () {
+
+  });
+
+  it('should should set error feedback in the kansenstatuut form', function () {
+
+  });
+
+  it('can submit the new card form', function () {
+
+  });
+
+  it('should not submit the new card form when there are errors', function () {
+
+  });
+
+  it('should should set error feedback in the new card form', function () {
+
+  });
+
+  it('can submit the price form', function () {
+
+  });
+
+  it('should not submit the price form when there are errors', function () {
+
+  });
+
+  it('should should set error feedback in the price form', function () {
 
   });
 });
