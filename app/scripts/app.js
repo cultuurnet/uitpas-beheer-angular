b42e7d13a15be9f74afdcf05db8ee0e70c18ee56
Larry Boltovskoi <larry.bolt@gmail.com>
2016-02-18 16:09:06 +0100

    UBR-433: On error pages scanning a nfc pass will work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -73,6 +73,7 @@ angular
       })
       .state('counter.main.error', {
         redirectOnScan: true,
+        onEnter: redirectOnScan,
         views: {
           'content@counter': {
             templateUrl: 'views/error.html',
