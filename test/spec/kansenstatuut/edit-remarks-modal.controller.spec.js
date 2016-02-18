58c18d034c7db013eb2cf8398a171ad202004999
Sander Vleugels <sandervleugels@gmail.com>
2016-02-18 15:36:23 +0100

    UBR-449: add tests for the modal controllers

---
+++ test/spec/kansenstatuut/edit-remarks-modal.controller.spec.js
@@ -0,0 +1,92 @@
+'use strict';
+
+describe('Controller: EditRemarksModalController', function () {
+
+  // load the controller's module
+  beforeEach(module('ubr.kansenstatuut'));
+
+  var controller, passholder, uibModalInstance, passholderService, $q, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, Passholder, $injector, $rootScope) {
+    uibModalInstance = {
+      close: jasmine.createSpy('uibModalInstance.close'),
+      dismiss: jasmine.createSpy('uibModalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('uibModalInstance.result.then')
+      }
+    };
+
+    passholderService = jasmine.createSpyObj('passholderService', ['updateRemarks']);
+    $q = $injector.get('$q');
+    scope = $rootScope.$new();
+    passholder = new Passholder(
+      {
+        passNumber: '01234567891234',
+        remarks: 'They gave remarks'
+      }
+    );
+
+    controller = $controller('EditRemarksModalController', {
+      passholder: passholder,
+      $uibModalInstance: uibModalInstance,
+      passholderService: passholderService,
+      $scope: scope
+    });
+  }));
+
+  it('can dismiss the modal', function () {
+    controller.cancelModal();
+    expect(uibModalInstance.dismiss).toHaveBeenCalled();
+  });
+
+  it('can clears async errors', function () {
+    controller.asyncError = { message: 'It did not do what is was supposed to do.' };
+    scope.$apply();
+
+    controller.remarks = 'Different text';
+    scope.$apply();
+
+    expect(controller.asyncError).toBeNull();
+    expect(controller.updatePending).toBeFalsy();
+  });
+
+  it('can update the remarks of the passholder', function () {
+    passholderService.updateRemarks.and.returnValue($q.when({
+      passNumber: '01234567891234',
+      remarks: 'text'
+    }));
+    var formStub = {
+      remarks: {
+        $valid: true
+      }
+    };
+
+    controller.updateRemarks(formStub);
+    expect(controller.updatePending).toBeTruthy();
+
+    scope.$digest();
+
+    expect(passholderService.updateRemarks).toHaveBeenCalled();
+    expect(controller.updatePending).toBeFalsy();
+    expect(uibModalInstance.close).toHaveBeenCalled();
+  });
+
+  it('can set an error when updating the remarks fails', function () {
+    passholderService.updateRemarks.and.returnValue($q.reject({
+      data: { message: 'It failed.' }
+    }));
+    var formStub = {
+      remarks: {
+        $valid: true
+      }
+    };
+
+    controller.updateRemarks(formStub);
+    scope.$digest();
+
+    expect(passholderService.updateRemarks).toHaveBeenCalled();
+    expect(controller.updatePending).toBeFalsy();
+    expect(controller.asyncError.message).toBe('It failed.');
+  });
+});
---
+++ test/spec/kansenstatuut/edit-school-modal.controller.spec.js
@@ -0,0 +1,120 @@
+'use strict';
+
+describe('Controller: EditSchoolModalController', function () {
+
+  // load the controller's module
+  beforeEach(module('ubr.kansenstatuut'));
+
+  var controller, passholder, uibModalInstance, passholderService, $q, scope, schools;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, Passholder, $injector, $rootScope) {
+    uibModalInstance = {
+      close: jasmine.createSpy('uibModalInstance.close'),
+      dismiss: jasmine.createSpy('uibModalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('uibModalInstance.result.then')
+      }
+    };
+
+    passholderService = jasmine.createSpyObj('passholderService', ['updateSchool']);
+    $q = $injector.get('$q');
+    scope = $rootScope.$new();
+    schools = [
+      {
+        id: 'unique-id-a',
+        name: 'School A'
+      },
+      {
+        id: 'unique-id-b',
+        name: 'School B'
+      },
+      {
+        id: 'unique-id-c',
+        name: 'School C'
+      },
+      {
+        id: 'unique-id-d',
+        name: 'School D'
+      }
+    ];
+    passholder = new Passholder(
+      {
+        passNumber: '01234567891234',
+        school: {
+          id: 'unique-id-a',
+          name: 'School A'
+        }
+      }
+    );
+
+    controller = $controller('EditSchoolModalController', {
+      passholder: passholder,
+      $uibModalInstance: uibModalInstance,
+      passholderService: passholderService,
+      $scope: scope,
+      schools: schools
+    });
+  }));
+
+  it('can dismiss the modal', function () {
+    controller.cancelModal();
+    expect(uibModalInstance.dismiss).toHaveBeenCalled();
+  });
+
+  it('can clears async errors', function () {
+    controller.asyncError = { message: 'It did not do what is was supposed to do.' };
+    scope.$apply();
+
+    controller.school = {
+      id: 'unique-id-b',
+      name: 'School B'
+    };
+    scope.$apply();
+
+    expect(controller.asyncError).toBeNull();
+    expect(controller.updatePending).toBeFalsy();
+  });
+
+  it('can update the school of the passholder', function () {
+    passholderService.updateSchool.and.returnValue($q.when({
+      passNumber: '01234567891234',
+      school: {
+        id: 'unique-id-a',
+        name: 'School A'
+      }
+    }));
+    var formStub = {
+      editSchool: {
+        $valid: true
+      }
+    };
+
+    controller.updateSchool(formStub);
+    expect(controller.updatePending).toBeTruthy();
+
+    scope.$digest();
+
+    expect(passholderService.updateSchool).toHaveBeenCalled();
+    expect(controller.updatePending).toBeFalsy();
+    expect(uibModalInstance.close).toHaveBeenCalled();
+  });
+
+  it('can set an error when updating the school fails', function () {
+    passholderService.updateSchool.and.returnValue($q.reject({
+      data: { message: 'It failed.' }
+    }));
+    var formStub = {
+      editSchool: {
+        $valid: true
+      }
+    };
+
+    controller.updateSchool(formStub);
+    scope.$digest();
+
+    expect(passholderService.updateSchool).toHaveBeenCalled();
+    expect(controller.updatePending).toBeFalsy();
+    expect(controller.asyncError.message).toBe('It failed.');
+  });
+});
