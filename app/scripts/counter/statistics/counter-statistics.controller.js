4657201ab4500445955669a4d3650bcfbd7abf84
zuuperman <nils.destoop@gmail.com>
2016-09-13 18:02:21 +0200

    UPS-1990: Added tooltips on the tables

--- app/scripts/counter/statistics/counter-statistics.controller.js
+++ app/scripts/counter/statistics/counter-statistics.controller.js
@@ -16,55 +16,103 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
   /*jshint validthis: true */
   var controller = this,
       info = {
-    'counter.statistics': {
-      pageTitle: 'Verkoop',
-      path: 'cardsales',
-      title: 'Verkochte kaarten',
-      plural_label: 'verkochte kaarten',
-      single_label: 'verkochte kaart',
-      type: 'Kopers',
-      profile: 'Profiel van de koper'
-    },
-    'counter.statistics.savings': {
-      pageTitle: 'Sparen',
-      path: 'checkins',
-      title: 'Gespaarde punten',
-      plural_label: 'gespaarde punten',
-      single_label: 'gespaard punt',
-      type: {
-        saved: 'Gespaarde punten',
-        active: 'Actieve spaarders',
-        new: 'Nieuwe spaarders'
+        'counter.statistics': {
+          pageTitle: 'Verkoop',
+          path: 'cardsales',
+          title: 'Verkochte kaarten',
+          pluralLabel: 'verkochte kaarten',
+          singleLabel: 'verkochte kaart',
+          type: {
+            buyers: {
+              'label': 'Kopers',
+              'help': 'Help tekst bij kopers'
+            }
+          },
+          profile: 'Profiel van de koper'
+        },
+        'counter.statistics.savings': {
+          pageTitle: 'Sparen',
+          path: 'checkins',
+          title: 'Gespaarde punten',
+          pluralLabel: 'gespaarde punten',
+          singleLabel: 'gespaard punt',
+          type: {
+            saved: {
+              'label': 'Gespaarde punten',
+              'help': 'Help tekst bij gespaarde punten'
+            },
+            active: {
+              'label': 'Actieve spaarders',
+              'help': 'Help tekst bij actieve spaarders'
+            },
+            new: {
+              'label': 'Nieuwe spaarders',
+              'help': 'Help tekst bij nieuwe spaarders'
+            }
+          },
+          profile: 'Profiel van de actieve spaarder'
+        },
+        'counter.statistics.exchange': {
+          pageTitle: 'Ruilen',
+          path: 'exchanges',
+          title: 'Omgeruilde voordelen',
+          pluralLabel: 'omgeruilde voordelen',
+          singleLabel: 'omgeruild voordeel',
+          type: {
+            active: {
+              label: 'Actieve ruilers',
+              help: 'help tekst bij actieve ruilers'
+            },
+            new: {
+              label: 'Nieuwe ruilers',
+              help: 'help tekst bij nieuwe ruilers'
+            },
+            transactions: {
+              label: 'Omgeruilde voordelen',
+              help: 'help tekst bij omgeruilde voordelen'
+            }
+          },
+          profile: 'Profiel van de actieve ruiler',
+          extraTable: {
+            title: 'aangeboden voordelen',
+            firstTableTitleLeft: 'Populairste ruilvoordelen',
+            firstTableTitleRight: 'Aantal ruilers'
+          }
+        },
+        'counter.statistics.mia': {
+          pageTitle: 'MIA\'s',
+          path: 'mias',
+          title: 'Actieve MIA\'s',
+          pluralLabel: 'actieve MIA\'s',
+          singleLabel: 'actieve MIA',
+          type: {
+            active: {
+              label: 'Actieve MIA\'s',
+              help: 'help tekst bij active mias'
+            },
+            saving: {
+              label: 'Sparende MIA\'s',
+              help: 'help tekst bij sparende mias'
+            },
+            exchanging: {
+              label: 'Ruilende MIA\'s',
+              help: 'help tekst bij ruilende mias'
+            }
+          },
+          profile: 'Profiel van MIA\'s',
+          extraTable: {
+            title: 'tickets aan kansentarief',
+            firstTableTitleLeft: 'Populairste tickets',
+            firstTableTitleRight: 'Aantal tickets',
+            secondTableTitleLeft: 'Populairste activiteiten',
+            secondTableTitleRight: 'Aantal spaarders'
+          }
+        }
       },
-      profile: 'Profiel van de actieve spaarder'
-    },
-    'counter.statistics.exchange': {
-      pageTitle: 'Ruilen',
-      path: 'exchanges',
-      title: 'Omgeruilde voordelen',
-      plural_label: 'omgeruilde voordelen',
-      single_label: 'omgeruild voordeel',
-      type: {
-        active: 'Actieve ruilers',
-        new: 'Nieuwe ruilers',
-        transactions: 'Omgeruilde voordelen'
-      },
-      profile: 'Profiel van de actieve ruiler'
-    },
-    'counter.statistics.mia': {
-      pageTitle: 'MIA\'s',
-      path: 'mias',
-      title: 'Actieve MIA\'s',
-      plural_label: 'actieve MIA\'s',
-      single_label: 'actieve MIA',
-      type: {
-        active: 'Actieve MIA\'s',
-        saving: 'Sparende MIA\'s',
-        exchanging: 'Ruilende MIA\'s'
-      },
-      profile: 'Profiel van MIA\'s'
-    }
-  };
+      helpTexts = {
+        average: 'Help tekst bij het gemiddelde',
+        yourBallie: 'Help tekst bij jouw balie'
+      };
 
   controller.loadingStatistics = true;
   controller.statistics = {};
@@ -75,6 +123,7 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
   controller.comparing = false;
   controller.titleStr = '';
   controller.profileStr = '';
+  controller.tooltip = d3.select('body').append('div').attr('class', 'graph-tooltip').style('opacity', 0);
 
   controller.loadDefaultDateRange = function() {
     var dateRange = counterService.getDefaultDateRange(),
@@ -158,6 +207,7 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
       controller.pageTitle = info[$state.current.name].pageTitle;
       controller.which = $state.current.name.split('.');
       controller.which = controller.which[controller.which.length - 1];
+      controller.extraTable = info[$state.current.name].extraTable;
 
       // Using settimeout to avoid waiting an extra cycle.
       setTimeout(function(){
@@ -267,7 +317,6 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
 
     // Make sure it's cleared.
     $graphWrap.empty();
-    var tooltip = d3.select('.counter-statistics-graph').append('div').attr('class', 'graph-tooltip').style('opacity', 0);
 
     // Line handler.
     line = d3.svg.line()
@@ -333,10 +382,10 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
           return yScale(parseInt(d.count, 10));
         })
         .on("mouseover", function(d) {
-          controller.showTooltip(d3.event, tooltip, d.count, d.date)
+          controller.showGraphTooltip(d3.event, d.count, d.date)
         })
         .on("mouseout", function() {
-          controller.hideTooltip(d3.event, tooltip);
+          controller.hideTooltip();
         });
 
     if (compare) {
@@ -361,10 +410,10 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
             return yScale(parseInt(d.count2, 10));
           })
           .on("mouseover", function(d) {
-            controller.showTooltip(tooltip, d.count2, d.date2)
+            controller.showGraphTooltip(d3.event, d.count2, d.date2)
           })
           .on("mouseout", function() {
-            controller.hideTooltip(tooltip);
+            controller.hideTooltip();
           });
 
     }
@@ -376,29 +425,46 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
   };
 
   /**
-   * Show the tooltip for a point on the graph.
+   * Show a tooltip for a point in the graph.
    */
-  controller.showTooltip = function(event, tooltip, total, date) {
+  controller.showGraphTooltip = function(event, total, date) {
+    var label = (total == 1 ? info[$state.current.name].singleLabel : info[$state.current.name].pluralLabel),
+      content = "<strong>" + date + "</strong><br/>" + " " + total + " " + label;
 
-    var label = (total == 1 ? info[$state.current.name].single_label : info[$state.current.name].plural_label);
+    controller.showTooltip(event, content);
+  }
 
-    tooltip.html("<strong>" + date + "</strong><br/>" + " " + total + " " + label);
+  /**
+   * Show a help tooltip.
+   */
+  controller.showHelpTooltip = function(event, key) {
+    if (helpTexts[key]) {
+      controller.showTooltip(event, helpTexts[key]);
+    }
+  }
+
+  /**
+   * Show a tooltip.
+   */
+  controller.showTooltip = function(event, content) {
+
+    controller.tooltip.html(content);
 
     // Tooltip needs to move first, if not getBoundingClientRect returns old info.
-    tooltip.style("left", (event.pageX + 5) + "px");
+    controller.tooltip.style("left", (event.pageX + 5) + "px");
 
-    var elementInfo = tooltip.node().getBoundingClientRect();
-    tooltip.style("top", (event.pageY - elementInfo.height) + "px");
+    var elementInfo = controller.tooltip.node().getBoundingClientRect();
+    controller.tooltip.style("top", (event.pageY - elementInfo.height) + "px");
 
-    tooltip.transition().duration(100).style("opacity", 1);
+    controller.tooltip.transition().duration(100).style("opacity", 1);
 
   };
 
   /**
    * Hide the tooltip.
    */
-  controller.hideTooltip = function(tooltip) {
-    tooltip.transition().duration(200).style("opacity", 0);
+  controller.hideTooltip = function() {
+    controller.tooltip.transition().duration(200).style("opacity", 0);
   };
 
   controller.loadDefaultDateRange();
