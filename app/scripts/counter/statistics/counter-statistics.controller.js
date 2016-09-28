7ee3d7ef234319ff9841051f30bb44c5034ff13a
seutje <seutje@gmail.com>
2016-09-28 14:57:43 +0100

    UPS-1982: Change 'last 7 days' to last 'week'.

--- app/scripts/counter/statistics/counter-statistics.controller.js
+++ app/scripts/counter/statistics/counter-statistics.controller.js
@@ -203,7 +203,7 @@ function CounterStatisticsController(counterService, $element, $state, $scope) {
         ranges: {
           'Today': [moment(), moment()],
           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+          'Last week': [moment().subtract(6, 'days'), moment()],
           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
           'This Month': [moment().startOf('month'), moment().endOf('month')],
           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
