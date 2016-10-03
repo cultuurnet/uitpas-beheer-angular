d4a2b8d3ad7baac500fe3030f92d2cdfe97d0fc2
zuuperman <nils.destoop@gmail.com>
2016-10-03 13:36:18 +0200

    UPS-1924: Change test descriptions + also test the return value of redirectOnScan

--- test/spec/utilities/uitpas-router.service.spec.js
+++ test/spec/utilities/uitpas-router.service.spec.js
@@ -30,11 +30,13 @@ describe('Service: UiTPAS router', function () {
     UiTPASRouter.redirectOnScanEnabled(true);
   }));
 
-  it('should change route when an UiTPAS is scanned and the active route allows auto redirects', function () {
+  it('should keep listening when redirectOnScan is called 2nd time', function () {
     $state.current = {
       redirectOnScan: true
     };
     router.redirectOnScanEnabled(true);
+    expect(router.redirectOnScanEnabled()).toBeTruthy();
+
     var number = '123456789';
     spyOn(router, 'go');
     rootScope.$emit('nfcNumberReceived', number);
@@ -43,7 +45,7 @@ describe('Service: UiTPAS router', function () {
     expect(router.go).toHaveBeenCalledWith(number);
   });
 
-  it('should keep listening when redirectOnScan is called 2nd time', function () {
+  it('should change route when an UiTPAS is scanned and the active route allows auto redirects', function () {
     $state.current = {
       redirectOnScan: true
     };
