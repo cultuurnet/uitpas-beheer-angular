8286ed21e6ac68544a3edbdfe5d3970edd756ba0
Thomas <thomasgoris@hotmail.be>
2020-04-06 08:14:14 +0200

    added semicolon

--- app/scripts/registration/passholder-register.controller.js
+++ app/scripts/registration/passholder-register.controller.js
@@ -44,7 +44,7 @@ function PassholderRegisterController (pass, $state, activeCounter, moment, coun
 
   // TODO: get from permissions instead of groups
   controller.canRegisterForeign = function () {
-    var hasPermission = activeCounter.groups.find(function(group){ return group === 'Mag pashouders uit buitenland registreren	'});
+    var hasPermission = activeCounter.groups.find(function(group){ return group === 'Mag pashouders uit buitenland registreren	';});
     return hasPermission !== undefined;
   };
 
