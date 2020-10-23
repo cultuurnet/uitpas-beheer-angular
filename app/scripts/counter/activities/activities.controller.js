8c3182ed7b77325616abe11d745c00ca8f69c412
dennis <dennis.deroover@optis.be>
2020-10-23 15:34:39 +0200

    fixes travis checks and removes unneeded code in template

--- app/scripts/counter/activities/activities.controller.js
+++ app/scripts/counter/activities/activities.controller.js
@@ -90,7 +90,7 @@ function ActivitiesController($http, activitiesService, counterService, DateRang
     if (str.length <= n) {
       return str;
     }
-    const subString = str.substr(0, n - 1); // the original check
+    var subString = str.substr(0, n - 1); // the original check
     return (useWordBoundary ? subString.substr(0, subString.lastIndexOf(' ')) : subString) + '...';
   }
 
