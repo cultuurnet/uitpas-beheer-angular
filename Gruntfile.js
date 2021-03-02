b19e7675d96b468658c07272090daff364a994f9
David <david.vangompel@gmail.com>
2021-03-02 09:42:11 +0100

    Add sourcemap files to build
    Refs #UPS-3299

--- Gruntfile.js
+++ Gruntfile.js
@@ -377,6 +377,12 @@ module.exports = function (grunt) {
       }
     },
 
+    uglify: {
+      options: {
+        sourceMap: true,
+      },
+    },
+
     // The following *-min tasks will produce minified files in the dist folder
     // By default, your `index.html`'s <!-- Usemin block --> will take care of
     // minification. These next options are pre-configured if you do not wish
